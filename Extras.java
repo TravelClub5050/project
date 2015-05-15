b4743fb4acb05da52ab30cfc31ac39a069ce9e8f
Kobrax <adam3308@stud.kea.dk>
2015-05-15 15:27:24 +0200

    List of atraction to each airport
    If I forgot to did something in my class just write to me, or do it by
    yourself if it's not a problem.

---
+++ Extras.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Extras
+{
+   List<String> CPH = Arrays.asList("Pick-up from airport to hotel", "Hotel in Copenhagen for time of tour", "Pick-up from hotel to airport", "One day tour of Copenhagen", "Two days tour of Copenhagen", "Sightseeing museums");
+   List<String> BKK = Arrays.asList("Pick-up from airport to hotel", "Hotel in Bangkok for time of tour", "Pick-up from hotel to airport", "Elephant tour for a day in Chiang Mai", "Two days tour of Bangkok");
+   List<String> CIA = Arrays.asList("Pick-up from airport to hotel", "Hotel in Roma for time of tour", "Pick-up from hotel to airport", "Sightseeing of the Vatican City", "One day tour of Roma", "Two days tour of Roma");
+   List<String> WAW = Arrays.asList("Pick-up from airport to hotel", "Hotel in Warsaw for time of tour", "Pick-up from hotel to airport", "One day tour of Warsaw", "Two days tour of Warsaw", "Sightseeing museums");
+   List<String> CDG = Arrays.asList("Pick-up from airport to hotel", "Hotel in Paris for time of tour", "Pick-up from hotel to airport", "One day tour of Paris", "Two days tour of Paris", "Sightseeing museums");
+   List<String> HND = Arrays.asList("Pick-up from airport to hotel", "Hotel in Tokyo for time of tour", "Pick-up from hotel to airport", "One day tour of Tokyo", "Two days tour of Tokyo");
+
+
+
+}
